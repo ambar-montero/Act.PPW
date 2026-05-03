fe2c92dfd77240c2a54c6dc12526d2cb671be181
ambar-montero <amonteroarreola@gmail.com>
2026-05-02 18:56:26 -0600

    Creación de estudiantes, nombre, correo y calificación

---
+++ Act8/Models/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Act8.Models
+{
+    public class Estudiante
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo no válido")]
+        public string Correo { get; set; } = string.Empty;
+
+        [System.ComponentModel.DataAnnotations.Range(0, 10,
+            ErrorMessage = "La calificación debe estar entre 0 y 10")]
+        public double Calificacion { get; set; }
+    }
+}
